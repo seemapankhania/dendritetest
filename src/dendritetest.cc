e20d45411ef77f2ee129f69c5af04476625ec4b3
Seema Pankhania <seempankhania@gmail.com>
2018-06-28 11:45:00 +0100

    "Initial commit"

---
+++ src/dendritetest.cc
@@ -0,0 +1,3 @@
+#include "dendritetest.h"
+
+int main(int argc, const char** argv) { return bdm::Simulate(argc, argv); }
---
+++ src/dendritetest.h
@@ -0,0 +1,30 @@
+#ifndef DENDRITETEST_H_
+#define DENDRITETEST_H_
+
+#include "biodynamo.h"
+
+namespace bdm {
+
+// Define compile time parameter
+template <typename Backend>
+struct CompileTimeParam : public DefaultCompileTimeParam<Backend> {};
+
+inline int Simulate(int argc, const char** argv) {
+  InitializeBiodynamo(argc, argv);
+
+  // Define initial model - in this example: single cell at origin
+  Cell cell({0, 0, 0});
+  cell.SetDiameter(30);
+  ResourceManager<>::Get()->push_back(cell);
+
+  // Run simulation for one timestep
+  Scheduler<> scheduler;
+  scheduler.Simulate(1);
+  
+  std::cout << "Simulation completed successfully!" << std::endl;
+  return 0;
+}
+
+} // namespace bdm
+
+#endif // DENDRITETEST_H_
